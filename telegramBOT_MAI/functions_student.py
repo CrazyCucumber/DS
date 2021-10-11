8020791281eddcad426e0fb11a3a035356613af1
CrazyCucumber <88565484+CrazyCucumber@users.noreply.github.com>
2021-10-11 18:09:19 +0300

    Update functions_student.py

--- telegramBOT_MAI/functions_student.py
+++ telegramBOT_MAI/functions_student.py
@@ -1,9 +1,96 @@
-# from datetime import datetime
 import telebot
+import psycopg2
 from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
-#
-#
+
+
+con = psycopg2.connect(
+  database="mai",
+  user="postgres",
+  password="1324",
+  host="127.0.0.1",
+  port="5432"
+)
+
+
+def registration(message):
+    lst = message.text.split(', ')
+    if len(lst) != 5:
+        bot.send_message(message.chat.id,
+                         f'{message.from_user.first_name}, неправильный формат сообщения')
+        return -1
+
+    name = str(lst[0])
+    group = lst[1]
+    task = lst[2]
+    variant = lst[3]
+    git = lst[4]
+
+    group_lst = ('212Б', '221Б', '214Б')
+    if group not in group_lst:
+        bot.send_message(message.chat.id,
+                         f'{message.from_user.first_name}, неправильно записана группа')
+        return -2
+
+    if int(task) < 1 or int(task) > 5:
+        bot.send_message(message.chat.id,
+                         f'{message.from_user.first_name}, неправильный номер задания\nВозможный номер задания от 1 до 5')
+        return -3
+
+    if int(variant) < 1 or int(variant) > 5:
+        bot.send_message(message.chat.id,
+                         f'{message.from_user.first_name}, неправильный номер варианта\nВозможный номер задания от 1 до 5')
+        return -4
+
+    if 'github.com/' not in git:
+        bot.send_message(message.chat.id,
+                         f'{message.from_user.first_name}, некоректная ссылка')
+        return -5
+
+    name = name.split(' ')
+    all_message = name + lst[1:5]
+
+    return all_message
+
+
+def DatabaseEntry(message):
+
+  #Подготовка SQL запроса
+  fam = "\'" + registration(message)[0] + "\'"
+  name = "\'" + registration(message)[1] + "\'"
+  otchestvo = "\'" + registration(message)[2] + "\'"
+  grp = "\'" + registration(message)[3] + "\'"
+  git = "\'" + registration(message)[6] + "\'"
+  SQLquery_to_entry_student = f"INSERT INTO STUDENT (fam,name,otchestvo,grp,task, var, git) VALUES ({fam}, {name}, {otchestvo}," \
+             f" {grp}, {registration(message)[4]}, {registration(message)[5]}, {git})"
+
+  # SQLquery_to_create_database = '''CREATE TABLE student (
+  #            id BIGSERIAL NOT NULL PRIMARY KEY,
+  #            fam VARCHAR(50),
+  #            name VARCHAR(50),
+  #            otchestvo VARCHAR(50),
+  #            grp VARCHAR(50),
+  #            task INT NOT NULL,
+  #            var INT NOT NULL,
+  #            git VARCHAR(200));'''
+
+  cur = con.cursor()
+
+#  try:
+#    cur.execute(SQLquery_to_create_database)
+#  except psycopg2.errors.DuplicateTable:
+#    pass
+#  finally:
+  cur.execute(SQLquery_to_entry_student)
+  con.commit()
+  con.close()
+
+  return bot.send_message(message.chat.id, f'{message.from_user.first_name}, Вы записаны в Базу Данных')
+
+
+
+
+# from datetime import datetime
 # def transformation_data():
 #     current_datetime = datetime.now()
 #     pos = str(current_datetime).rfind('.')
@@ -71,34 +158,3 @@ bot = telebot.TeleBot(TOKEN)
 #     return bot.send_message(message.chat.id,
 #                             f'{message.from_user.first_name}, ты записан!')
 #
-def registration(message):
-    lst = message.text.split(', ')
-    if len(lst) != 5:
-        bot.send_message(message.chat.id,
-                                     f'{message.from_user.first_name}, не правильный формат сообщения')
-        return -1
-    name = lst[0]
-    group = lst[1]
-    task = lst[2]
-    variant = lst[3]
-    git = lst[4]
-
-    group_lst = ('212Б', '221Б', '214Б')
-    if group not in group_lst:
-        bot.send_message(message.chat.id,
-                         f'{message.from_user.first_name}, не правильный формат сообщения')
-        return -2
-
-    if int(task) < 1 or int(task) > 5:
-        print('Неправильный номер задания')
-        print('Возможный номер задания от 1 до 5')
-        return -3
-
-    if int(variant) < 1 or int(variant) > 5:
-        print('Неправильный номер варианта')
-        print('Возможный номер номер варианта от 1 до 5')
-        return -4
-
-    if 'github.com/' not in git:
-        print('Некорркетная ссылка')
-        return -5
